61e1b8838ad6f581180f0c3f37c00406aab1f297
Abhay Agrawal <abhayemailid@gmail.com>
2014-08-21 15:28:39 +0530

    CRUD for resources: projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,47 @@
+class ProjectsController < ApplicationController
+  def index
+  	@projects = Project.all
+  end
+
+  def new
+  	@project = Project.new
+  end
+
+  def create
+  	@project = Project.new(project_params)
+  	if @project.save
+  		# redirect to projcet page?
+  		redirect_to @project, notice: "Project created!"
+  	else
+  		# show error
+  		render 'new'
+  	end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(project_params)
+      redirect_to @project, notice: "Project updated."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Project.find(params[:id]).destroy
+    redirect_to projects_path, alert: "Project deleted."
+  end
+
+  private
+  		def project_params
+  			params.require(:project).permit(:name, :description)
+  		end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def title(*parts)
+		if !parts.empty?
+			content_for :title do 
+				(parts << "Ticketee").join(" - ")
+			end
+		end
+	end
 end
