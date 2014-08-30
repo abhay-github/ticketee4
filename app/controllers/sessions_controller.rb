cd1075d97817852e044a0b3f62fd2c2ea21e2fcc
Abhay Agrawal <abhayemailid@gmail.com>
2014-08-30 22:35:44 +0530

    sign in/out, like mhartl

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,31 @@
 class SessionsController < ApplicationController
+
+  include SessionsHelper
+
   def new
   end
 
   def create
   	usr = User.find_by(email: params[:session][:email])
   	if usr.authenticate(params[:session][:password])
-  		# set a cookie on browser
-  		# session['ticketee'] = 
+      sign_in usr
   		# route to user profile
-  		redirect_to usr, notice: "Welcome, #{usr.name}!"
+      # redirect_to usr, notice: "Welcome, #{usr.name}!"
+  		redirect_to usr, notice: "Welcome, #{@current_user.name}!"
   	else
   		render 'new', alert: "Username / Password do not match."
   	end
   end
 
   def destroy
+    sign_out
+    redirect_to root_path
   end
+
+  private
+
+    def sign_out
+      cookies.delete(:ticketee_token)
+      @current_user = nil
+    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,25 @@
 module SessionsHelper
-	def signed_in?
-		false
-	end
+	def sign_in(usr)
+      token = SecureRandom.urlsafe_base64
+      cookies[:ticketee_token] = token
+      hashed_token = Digest::SHA1.hexdigest(token)
+      usr.update(remember_token: hashed_token)
+      @current_user = usr
+    end
+
+    def signed_in?
+    	if current_user.nil?
+    		return false;
+    	else
+    		return true
+    	end
+    end
+
+    def current_user
+    	@current_user ||= User.find_by(remember_token: hashed(cookies[:ticketee_token]))
+    end
+
+    def hashed(token)
+    	Digest::SHA1.hexdigest(token.to_s)
+    end
 end
